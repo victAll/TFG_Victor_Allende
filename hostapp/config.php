b1887e7f019f49a30255abd766e0fc29a7fdf9fa
DESKTOP-6IKIV4K\victo <victormanuel.allende@gmail.com>
2023-04-12 15:09:18 +0200

    config.php

--- hostapp/config.php
+++ hostapp/config.php
@@ -2,6 +2,7 @@
 define("urlsite", "http://localhost/tfg/hostapp/");
 
 define("ROOT", __DIR__ ."/");
+//definicion de ruta base parar todo el proyecto
 define("HTTP", ($_SERVER["SERVER_NAME"] == "localhost")
    ? "http://localhost/tfg/hostapp/"
    : "http://your_site_name.com/"
